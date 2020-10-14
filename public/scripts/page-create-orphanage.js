a067474c027ec68fa02ea60f1a9f6f404596456f
Luanna Garla <luagarla@gmail.com>
2020-10-14 19:30:30 -0300

    finished page-create-orphanage

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -9,52 +9,83 @@ const icon = L.icon({
   iconUrl: "./public/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2],
 });
 
 let marker;
 
 //create and add marker
-map.on('click', (event) => {
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
+map.on("click", (event) => {
+  const lat = event.latlng.lat;
+  const lng = event.latlng.lng;
 
-    document.querySelector('[name=lat]').value = lat
-    document.querySelector('[name=lng]').value = lng
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
-    //remove icon 
-    marker && map.removeLayer(marker)
+  //remove icon
+  marker && map.removeLayer(marker);
 
-    // add icon layer
-    marker = L.marker([lat, lng], {icon})
-    .addTo(map)
-})
-
+  // add icon layer
+  marker = L.marker([lat, lng], { icon }).addTo(map);
+});
 
 //add campo photos
 
-function addPhotoField(){
-    // pegar o container de fotos |#images
-    const container = document.querySelector('#images')
+function addPhotoField() {
+  // pegar o container de fotos |#images
+  const container = document.querySelector("#images");
 
-    // pegar o container para duplicar |.new-upload
-    const fieldContainer = document.querySelectorAll('.new-upload')
+  // pegar o container para duplicar |.new-upload
+  const fieldContainer = document.querySelectorAll(".new-upload");
 
-    //realizar o clone da última imagem adicionada
-    const newFieldContainer = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+  //realizar o clone da última imagem adicionada
+  const newFieldContainer = fieldContainer[fieldContainer.length - 1].cloneNode(
+    true
+  );
 
-    //verificar se está vazio, se sim, n adiconar 
-    const input = newFieldContainer.children[0]
+  //verificar se está vazio, se sim, n adiconar
+  const input = newFieldContainer.children[0];
 
-    if (input.value == ""){
-        return 
-    }
+  if (input.value == "") {
+    return;
+  }
 
-    //limpar campo 
-    newFieldContainer.children[0].value = ''
+  //limpar campo
+  newFieldContainer.children[0].value = "";
 
-    //adicionar o clone ao container |#images
-    container.appendChild(newFieldContainer)
+  //adicionar o clone ao container |#images
+  container.appendChild(newFieldContainer);
 }
 
-// function deleteField
+function deleteField(event) {
+  const span = event.currentTarget;
+
+  const fieldContainer = document.querySelectorAll(".new-upload");
+
+  if (fieldContainer.length <= 1) {
+    //limpar o valor do campo
+    span.parentNode.children[0].value = "";
+    return;
+  }
+
+  //deletar o campo
+  span.parentNode.remove();
+}
+
+// selecionar sim ou não
+
+function toggleSelect(event) {
+  // retirar a class .active (dos botoes)
+  document
+    .querySelectorAll(".button-select button")
+    .forEach((button) => button.classList.remove("active"));
+
+  // colocar a class .active nesse botao clicado
+  const button = event.currentTarget;
+  button.classList.add("active");
+
+  //atualizar o input hidden com valor selecionado
+  const input = document.querySelector('[name="open-on-weekends"]');
+
+  //verificar se sim ou não
+  input.value = button.dataset.value;
+}
