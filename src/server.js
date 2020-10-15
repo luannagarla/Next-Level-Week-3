9f1e0a2650932113aa45c8a3baa0e9c3a9fa2759
Luanna Garla <luagarla@gmail.com>
2020-10-15 19:19:18 -0300

    Server update

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 // importar a biblioteca
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
 //iniciando o express
-const server = express()
+const server = express();
 
 //crir uma rota
-server.get('/', (request, response) => {
-    return response.send('uhuu')
-})
+server
+  //pasta com arquivos estáticos
+  .use(express.static("public")) 
+
+  //configurar template engina
+  .set('views', path.join(__dirname, "views"))
+  .set('view engine', 'hbs')
+
+  //criação de rota
+  .get("/", (request, response) => {
+    // console.log(request.query) - posso ver oq foi registrado na barra
+    return response.render('index')
+  });
 
 //ligar o servidor
-server.listen(5500)
+server.listen(5500);
